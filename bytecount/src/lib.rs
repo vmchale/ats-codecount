99700200198ca72337f4c0e89299e2176095d274
Vanessa McHale <vamchale@gmail.com>
2019-10-16 16:10:27 -0500

    Add memchr2 and memchr3 exports

--- bytecount/src/lib.rs
+++ bytecount/src/lib.rs
@@ -3,14 +3,23 @@ extern crate libc;
 extern crate memchr;
 
 use libc::c_int;
+use libc::c_uint;
 use libc::size_t;
 use std::slice;
 
 #[no_mangle]
-pub extern "C" fn memchr2_(bytes: *const u8, b0: char, b1: char, bufsz: size_t) -> Option<usize> {
+pub extern "C" fn memchr2(bytes: *const u8, b0: char, b1: char, bufsz: size_t) -> Option<c_uint> {
     unsafe {
         let byte_slice = slice::from_raw_parts(bytes, bufsz);
-        memchr::memchr2(b0 as u8, b1 as u8, byte_slice)
+        memchr::memchr2(b0 as u8, b1 as u8, byte_slice).map(|x| x as c_uint)
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn memchr3(bytes: *const u8, b0: char, b1: char, b2: char, bufsz: size_t) -> Option<c_uint> {
+    unsafe {
+        let byte_slice = slice::from_raw_parts(bytes, bufsz);
+        memchr::memchr3(b0 as u8, b1 as u8, b2 as u8, byte_slice).map(|x| x as c_uint)
     }
 }
 
