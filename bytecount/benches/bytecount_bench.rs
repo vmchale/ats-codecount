344873ddc7b265f2d0a1e9dd675f76b3fb2bac42
Vanessa McHale <vamchale@gmail.com>
2019-10-18 14:10:14 -0500

    Add unbuffered benchmark for enlightenment

--- bytecount/benches/bytecount_bench.rs
+++ bytecount/benches/bytecount_bench.rs
@@ -8,6 +8,14 @@ use criterion::Criterion;
 use std::fs::File;
 use std::io::prelude::*;
 
+fn file_lines_nobuf(fp: &str) -> usize {
+    let mut f = File::open(fp).unwrap();
+    let mut buffer = String::new();
+
+    f.read_to_string(&mut buffer).unwrap();
+    bytecount::count(&buffer.as_bytes(), b'\n')
+}
+
 fn file_lines(fp: &str) -> usize {
     let mut f = File::open(fp).unwrap();
     let mut buffer = [0; 32 * 1024];
@@ -25,11 +33,16 @@ fn file_lines(fp: &str) -> usize {
     linecount
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn buffered_benchmark(c: &mut Criterion) {
     c.bench_function("bytecount (sqlite3.c)", |b| {
         b.iter(|| file_lines("../test/data/sqlite3.c"))
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn unbuffered_benchmark(c: &mut Criterion) {
+    c.bench_function("bytecount (unbuffered) (sqlite3.c)", |b| {
+        b.iter(|| file_lines_nobuf("../test/data/sqlite3.c"))
+    });
+}
+criterion_group!(benches, buffered_benchmark, unbuffered_benchmark);
 criterion_main!(benches);
