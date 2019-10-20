fbc7c97fb6a243b1ece1b8696500dbd7c263fbc3
Vanessa McHale <vamchale@gmail.com>
2019-10-19 23:05:31 -0500

    Semi-more-valid c?

--- test/data/pathological.c
+++ test/data/pathological.c
@@ -1,19 +1,21 @@
 /* block comments don't nest /* */
 
+#include <stdio.h>
+
 static int test_format_dir_put(char *dir) {
     snprintf(buf, PATH_MAX, "rm -f %s/*\n", dir);
 }
 
 char silly_char(void) {
     // do count this line
-    '"' // don't count this line
+    return '"'; // don't count this line
 }
 
 int always_return_zero(void) { /* begin comment
                                   second bit of comment
                                   */
 
-    0
+    return 0;
 }
 
 void silly_switch(char c) {
