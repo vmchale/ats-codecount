25cf47130a3e66e40b4dc50183f6c119e6910a04
Vanessa McHale <vamchale@gmail.com>
2019-11-01 12:58:16 -0500

    Add benchmark of loop/iterator for curiosity

--- bytecount/benches/bytecount_bench.rs
+++ bytecount/benches/bytecount_bench.rs
@@ -33,16 +33,45 @@ fn file_lines(fp: &str) -> usize {
     linecount
 }
 
+fn count_lines(buf: &[u8]) -> usize {
+    let buf_loop = buf.iter();
+    buf_loop.filter(|c| **c == b'\n').count()
+}
+
+fn file_lines_loop(fp: &str) -> usize {
+    let mut f = File::open(fp).unwrap();
+    let mut buffer = [0; 32 * 1024];
+    let mut linecount = 0;
+    loop {
+        let read_result = f.read(&mut buffer).unwrap();
+        match read_result {
+            0 => break,
+            res => {
+                let sub_buffer = &buffer[0..res];
+                linecount = linecount + count_lines(&sub_buffer);
+            }
+        };
+    }
+    linecount
+}
+
 fn buffered_benchmark(c: &mut Criterion) {
     c.bench_function("bytecount (sqlite3.c)", |b| {
-        b.iter(|| file_lines("../test/data/sqlite3.c"))
+        b.iter(|| file_lines("../bench/data/sqlite3.c"))
     });
 }
 
 fn unbuffered_benchmark(c: &mut Criterion) {
     c.bench_function("bytecount (unbuffered) (sqlite3.c)", |b| {
-        b.iter(|| file_lines_nobuf("../test/data/sqlite3.c"))
+        b.iter(|| file_lines_nobuf("../bench/data/sqlite3.c"))
     });
 }
-criterion_group!(benches, buffered_benchmark, unbuffered_benchmark);
+
+fn loop_benchmark(c: &mut Criterion) {
+    c.bench_function("bytecount (iterator)", |b| {
+        b.iter(|| file_lines_loop("../bench/data/sqlite3.c"))
+    });
+}
+
+criterion_group!(benches, buffered_benchmark, unbuffered_benchmark, loop_benchmark);
 criterion_main!(benches);
